f5a21b3a7c1a6a338b0e629430b9943431da77cb
Kelly Lougheed <kellylougheed@gmail.com>
2018-07-16 16:53:21 +0000

    Make script more clearly illustrate collision information

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-console.log("loaded!")
-
-
-
 const player = document.querySelector('#player')
 const theball = document.querySelector('#theball')
 
-player.addEventListener('collide', function (e) {
-    console.log('Player has collided with body #' + e.detail.body.id);
-
-    e.detail.target.el;  // Original entity (playerEl).
-    e.detail.body.el;    // Other entity, which playerEl touched.
+player.addEventListener('collide', e => {
+    console.log("Collider (below):");
+    console.log(e.detail.target.el);
+    console.log("Collider ID: " + e.detail.target.el.id);
+    
+    console.log("Collided with (below):");
+    console.log(e.detail.body.el);
+    
+    e.detail.target.el;  // Original entity, the collider
+    e.detail.body.el;    // Entity that was collided with
     e.detail.contact;    // Stats about the collision (CANNON.ContactEquation).
     e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
 });
-
