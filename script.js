d195df496c73581a2b89953eb37e34e05ed6c8dd
Kelly Lougheed <kellylougheed@gmail.com>
2018-07-17 19:18:42 +0000

    clarify

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const player = document.querySelector('#player')
 const theball = document.querySelector('#ball')
 
 player.addEventListener('collide', e => {
-    console.log(e.detail.target.el.id + " collided with " + e.detail.body.el);
+    console.log(e.detail.target.el.id + " collided with " + e.detail.body.el.id);
     
-    e.detail.target.el;  // Original entity, the collider
+    e.detail.target.el;  // Original entity (subject of event listener)
     e.detail.body.el;    // Entity that was collided with
     e.detail.contact;    // Stats about the collision (CANNON.ContactEquation).
     e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
